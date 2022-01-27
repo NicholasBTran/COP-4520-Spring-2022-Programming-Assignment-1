356cf89263436976c140c95a6ae7bfb97dcecac9
NicholasBTran <nicholasbtran@gmail.com>
2022-01-27 18:57:43 -0500

    Cleaned up some of the code

--- COP-4520-Spring-2022-Programming-Assignment-1/src/Primes.java
+++ COP-4520-Spring-2022-Programming-Assignment-1/src/Primes.java
@@ -1,85 +1,24 @@
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
-// source: https://www.geeksforgeeks.org/sieve-of-eratosthenes/
-class SieveOfEratosthenes {
-	static long sum = 0;
-	static long count = 0;
 
-	void sieveOfEratosthenes(int n) {
-		// Create a boolean array
-		// "prime[0..n]" and
-		// initialize all entries
-		// it as true. A value in
-		// prime[i] will finally be
-		// false if i is Not a
-		// prime, else true.
-		boolean prime[] = new boolean[n + 1];
-		for (int i = 0; i <= n; i++)
-			prime[i] = true;
-
-		for (int p = 2; p * p <= n; p++) {
-			// If prime[p] is not changed, then it is a
-			// prime
-			if (prime[p] == true) {
-				// Update all multiples of p
-				for (int i = p * p; i <= n; i += p)
-					prime[i] = false;
-			}
-		}
-
-		// Sum all prime numbers
-		for (int i = 2; i <= n; i++) {
-			if (prime[i] == true) {
-				count++;
-				sum += i;
-			}
-		}
-	}
-
-	// Driver Code
-	public static void main(String args[]) {
-		int n = 100000000;
-		SieveOfEratosthenes g = new SieveOfEratosthenes();
-		g.sieveOfEratosthenes(n);
-	}
-}
-
+// Adapted from SieveOfEratosthenes (source: https://www.geeksforgeeks.org/sieve-of-eratosthenes/)
 class NewThread extends Thread {
 	static AtomicInteger counter = new AtomicInteger(2);
-	static AtomicInteger counter2 = new AtomicInteger(2);
-	static AtomicInteger numPrime = new AtomicInteger();
-	static AtomicLong sumPrime = new AtomicLong();
 	static final int n = 100000000;
 	static boolean prime[] = new boolean[n + 1];
 
 	public void run() {
-		/*
-		 * for (int p = 2; p * p <= n; p++) { // If prime[p] is not changed, then it is
-		 * a // prime if (prime[p] == true) { // Update all multiples of p for (int i =
-		 * p * p; i <= n; i += p) prime[i] = false; } }
-		 */
-
 		for (int p = counter.getAndIncrement(); p * p <= n; p = counter.getAndIncrement()) {
-			// If prime[p] is not changed, then it is a
-			// prime
+			// If prime[p] is not changed, then it is a prime
 			if (prime[p] == true) {
 				// Update all multiples of p
 				for (int i = p * p; i <= n; i += p)
 					prime[i] = false;
 			}
 		}
-
-		/*
-		for (int i = counter2.getAndIncrement(); i <= n; i = counter2.getAndIncrement()) {
-			if (prime[i] == true) {
-				numPrime.incrementAndGet();
-				sumPrime.addAndGet(i);
-			}
-		}
-		*/
-
 	}
 }
 
@@ -89,15 +28,13 @@ public class Primes {
 		long startTime = System.currentTimeMillis();
 		long sum = 0;
 		long count = 0;
-
-		// SieveOfEratosthenes.main(null);
-		// System.out.println(SieveOfEratosthenes.count);
-
 		ArrayList<Thread> threads = new ArrayList<>();
 
+		// Initialize array used for prime checking
 		for (int i = 0; i <= NewThread.n; i++)
 			NewThread.prime[i] = true;
 
+		// Create and run threads
 		for (int i = 0; i < 8; i++) {
 			NewThread t = new NewThread();
 			t.setName("MyThread-" + i);
@@ -113,22 +50,45 @@ public class Primes {
 			}
 		}
 
-		
+		// Calculate sum and total count of primes
 		for (int i = 2; i <= NewThread.n; i++) {
 			if (NewThread.prime[i] == true) {
 				count++;
 				sum += i;
 			}
 		}
-		System.out.println(count);
+
+		// Find the top ten prime numbers
+		int topTen[] = new int[10];
+		int index = 0;
+		for (int i = NewThread.n; i > 0 && index < 10; i-- ) {
+			if (NewThread.prime[i] == true) {
+				topTen[index++] = i;
+			}
+		}
 		
-
-		//System.out.println(NewThread.numPrime);
-
+		String lastTenPrimes = "";
+		for (int i = 9; i >= 0; i--) {
+			// System.out.print(topTen[i] + " ");
+			lastTenPrimes += topTen[i] + " ";
+		}
+		
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
-		System.out.println("Execution time = " + elapsedTime);
 
+		// Write output to file
+		try {
+			String str = "" + elapsedTime + " " + count + " " + sum + "\n" + lastTenPrimes;
+			System.out.println(str);
+		    FileOutputStream outputStream = new FileOutputStream("primes.txt");
+		    byte[] strToBytes = str.getBytes();
+		    outputStream.write(strToBytes);
+
+		    outputStream.close();
+		} catch (Exception e) {
+			
+		}
+		
 	}
 
 }
